6344b9ace7c8257686e7e4a831b9a0567c498333
RajAditya01 <adityavishal903@gmail.com>
2023-07-16 16:10:10 +0530

    .

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+const express = require("express");
+const { read } = require("fs");
+const { get } = require("http");
 
+const app = express();
+
+app.get(route, callback);
+
+// API 
+//CRUD OPERATION
+// get - read
+// post - create 
+// put - update 
+// delete - delet
